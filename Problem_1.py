777c1145d0d15b73c8d3887f42fdab73af255ed3
irrawaddy28 <xad890@gmail.com>
2025-08-18 18:41:40 -0700

    completed all changes

---
+++ Problem_1.py
@@ -0,0 +1,130 @@
+'''
+139 Word Break
+https://leetcode.com/problems/word-break/description/
+
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example 1:
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+
+Constraints:
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+
+Solution:
+1. Recursion
+https://youtu.be/St-WLFHcRzw?t=2142
+Time: O(2^N)  Space:  O(N)
+
+2. DP
+Create an empty array of len N+1 to store dp values.
+dp[i] = 1 if string[0:i] = string[0...i-1] is breakable/partitioned
+      = 0, otherwise
+Eg. if s = 'leetcode' and dp[4] = True, it means "leet" is breakable.
+index 0 1 2 3 4 5 6 7
+char  l e e t c o d e
+Thus, dp[i] = prefix in the string (prior to current index i) is breakabale
+
+Initialize, dp[0] = 1 (since string[0:0] = "" (empty string) = nothing to break)
+dp[i] = 1 if for any j in 0:i-1, dp[j] == 1 and s[j:i] in h
+
+dp[j] = 1 means prefix = s[0:j] is breakable
+s[j:i] in h means suffix = s[j:i] is present in dictionary
+
+We iterative fill dp[i] for each i = 1,...,N
+if dp[N] = True (s[0:N] = s[0...N-1] is breakable)
+         = False (s[0...N-1] is not breakable)
+https://youtu.be/St-WLFHcRzw?t=3207
+Time: O(N^3)  Space:  O(MK + N) (M = words in dict, K = avg len of words in dict, N = length of dp array)
+
+Note: If we treat the creation of a copy of the suffix (using the following statement) as O(1), then time complexity = O(N^2)
+suffix = s[j:i]
+'''
+from typing import List
+
+def wordBreak_recursion(s: str, wordDict: List[str]) -> bool:
+    def recurse(s, index):
+        if index == N:
+            return True
+        found = False
+        for i in range(index, N):
+            substring = s[index:i+1]
+            if substring in h:
+                found = recurse(s, i+1)
+                if found:
+                    break
+        return found
+
+    if not s:
+        return True
+    if not wordDict:
+        return False
+    N = len(s)
+    h = set()
+    for word in wordDict:
+        h.add(word)
+    return recurse(s, 0)
+
+def wordBreak_dp(s: str, wordDict: List[str]) -> bool:
+    if not s:
+        return True
+    if not wordDict:
+        return False
+    N = len(s)
+    h = set()
+    for word in wordDict:
+        h.add(word)
+    dp = [False]*(N+1)
+    dp[0] = True
+    for i in range(N+1): # i = last index, iterate i from 1 to N, O(N)
+        for j in range(i): # j = start index of suffix, O(N)
+            if dp[j] == True: # prefix = s[0...j-1] is breakable
+                suffix = s[j:i] # suffix = s[j...i-1], O(N) (copying N chars)
+                if suffix in h:
+                    dp[i] = True
+                    break
+            else: # if prefix is not breakable, go to next j
+                pass
+        i += 1
+    return dp[N]
+
+def run_wordBreak():
+    tests = [("leetcode", ["leet","code"], True),
+             ("leetcode", ["l", "le", "etc", "o", "de"], True),
+             ("leetcode", ["l", "le", "etc", "o"], False),
+             ("applepenapple", ["apple","pen"], True),
+             ("catsandog", ["cats","dog","sand","and","cat"], False),
+    ]
+    for test in tests:
+        s, wordDict, ans = test[0], test[1], test[2]
+        print(f"\nString = {s}")
+        print(f"Dictionary = {wordDict}")
+        for method in ['recursion', 'dp']:
+            if method == "recursion":
+                breakable = wordBreak_recursion(s, wordDict)
+            elif method == "dp":
+                breakable = wordBreak_dp(s, wordDict)
+            print(f"Method {method}: Is word break possible? {breakable} ")
+            success = (ans == breakable)
+            print(f"Pass: {success}")
+            if not success:
+                print("Failed")
+                return
+
+run_wordBreak()
---
+++ Problem_2.py
@@ -0,0 +1,113 @@
+'''
+62 Unique Paths
+https://leetcode.com/problems/unique-paths/description/
+
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2^109.
+
+
+Example 1:
+Input: m = 3, n = 7
+Output: 28
+
+Example 2:
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+
+Constraints:
+1 <= m, n <= 100
+
+Solution:
+1. Recursion:
+Pick row or pick column.
+https://youtu.be/St-WLFHcRzw?t=217
+Time: O(2^MN), Space: O(1)
+
+2. Bottom-up DP 1:
+Initialize a dp matrix of size M x N. dp[i][j] = no. of ways to reach the end starting rom cell i,j. Then dp[i][j] = dp of cell at right + dp of cell at bottom = dp[i][j+1] + dp[i+1][j].
+Start from the right bottom corner by setting dp[m-1][n-1] = 1, since if you are at m-1,n-1 and you have to reach the same cell, there is only 1 way of reaching there. We can't say 0 (since that would mean we cannot reach that cell). Continue this process until you reach top left.
+https://youtu.be/St-WLFHcRzw?t=683
+Time: O(MN), Space: O(MN)
+
+3. Bottom-up DP 2:
+Save space by using only 1-D array of DP. We can't do this with just two variables which track bottom and right values of dp.
+https://youtu.be/St-WLFHcRzw?t=1509
+Time: O(MN), Space: O(N)
+'''
+
+def uniquePaths_recur(m: int, n: int) -> int:
+    def recurse(m, n, row, col):
+        # base
+        if row == m or col == n:
+            return 0
+
+        if row == m-1 and col == n-1:
+            return 1
+
+        # logic
+        # move right
+        case1 = recurse(m, n, row, col+1)
+
+        # move low
+        case2 = recurse(m, n, row+1, col)
+
+        return case1 + case2
+    if m == 0 and n == 0:
+        return 0
+    return recurse(m, n, 0, 0)
+
+def uniquePaths_BottomUp1(m: int, n: int) -> int:
+    if m == 0 and n == 0:
+        return 0
+    dp = [ [0]*(n+1) for _ in range(m+1)]
+    dp[m-1][n-1] = 1
+    for i in range(m-1,-1,-1):
+        for j in range(n-1,-1,-1):
+            if i == m-1 and j == n-1:
+                continue
+            dp[i][j] = dp[i][j+1] + dp[i+1][j]
+    return dp[0][0]
+
+def uniquePaths_BottomUp2(m: int, n: int) -> int:
+    if m == 0 and n == 0:
+        return 0
+
+    # Fill dp = [1,1,1,1..] which represents the last row of DP matrix
+    dp =[1]*n
+
+    # Start from 2nd last row and 2nd last col (m-2, n-2)
+    for i in range(m-2,-1,-1): # why start with m-2?
+        for j in range(n-2,-1,-1): # why start with n-2?
+            right = dp[j+1]
+            bottom = dp[j]
+            dp[j] = right + bottom
+    return dp[0]
+
+def run_uniquePaths():
+    tests = [(3,3,6), (4,4,20), (3,7,28), (3,2,3), (2,2,2), (1,1,1), (0,0,0)]
+    for test in tests:
+        m, n, ans = test[0], test[1], test[2]
+        print(f"\nGrid rows = {m}, columns = {n}")
+        for method in ['recursion', 'DP1', 'DP2']:
+            if method == 'recursion':
+                ways = uniquePaths_recur(m,n)
+            elif method == 'DP1':
+                ways = uniquePaths_BottomUp1(m,n)
+            elif method == 'DP2':
+                ways = uniquePaths_BottomUp2(m,n)
+            print(f"Method {method}: Num ways to reach last cell = {ways}")
+            success = (ans == ways)
+            print(f"Pass: {success}")
+            if not success:
+                print("Failed")
+                return
+
+
+run_uniquePaths()
